94ecdc9b2b5c021a540edb8386d02c314ce2788d
brian <brian@lucky.com.hk>
2022-02-18 00:19:36 +0800

    blah

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static double Add(double a, double b)
+{
+    return a * b;
+}
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+ReadLine(); // wait for user to press ENTER
